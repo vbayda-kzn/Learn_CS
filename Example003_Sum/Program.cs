cd76d6f45515071e35e2c5049a1b7792222c817f
Vladimir_B <vladimir.baida@gmail.com>
2022-06-23 10:46:37 +0300

    add random in Example002

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Сложение двух чисел
 
-int numberA = 3;
-int numberB = 5;
+int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA + numberB);
