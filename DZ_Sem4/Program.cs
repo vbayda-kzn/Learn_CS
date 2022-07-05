177baca5ab810e01ea3f0a1cc7aa76afed422b94
Vladimir_B <vladimir.baida@gmail.com>
2022-07-05 21:00:49 +0300

    Задачки из 4-го семинара и ДЗ к нему

---
+++ DZ_Sem4/Program.cs
@@ -0,0 +1,107 @@
+﻿// Домашняя работа по C# - Семинар 3. Байда Владимир
+
+//--------------------------------------------------------------------------------------------------------------------------
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+/*void Zadacha25 ()
+{
+    Console.Write("Введите число А: ");
+    int numberA = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число B: "); 
+    int numberB = Convert.ToInt32(Console.ReadLine());
+    
+    Console.WriteLine($"Если мы случайно число {numberA} возведем в степень {numberB}, то получим {myPow(numberA, numberB)}!");
+
+    int myPow (int numberA, int numberB)
+    {
+        return Convert.ToInt32(Math.Pow(numberA, numberB));
+    }
+}
+Zadacha25();
+*/
+
+
+//--------------------------------------------------------------------------------------------------------------------------
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+/*void Zadacha27 ()
+{
+    Console.Write("Введите число: ");
+    string number = Console.ReadLine();
+    
+    Console.WriteLine($"Сумма цифр в числе {number} равна {SumNumbers(number)}!");
+
+    int SumNumbers (string number)
+    {
+        int sum = 0;    
+        for (int i = 0; i < number.Length; i++)
+        {
+            sum += Convert.ToInt32(Convert.ToString((number[i])));
+        }
+        return sum;
+    }
+}
+Zadacha27();
+*/
+
+//--------------------------------------------------------------------------------------------------------------------------
+// Задача 29: Напишите программу, которая задаёт массив из 8 случайных целых чисел и выводит отсортированный по модулю массив.
+
+void Zadacha29()
+{
+    int[] array = new int[8];
+    Random rand = new Random();
+    
+    FillRandArray(array);
+    
+    Console.WriteLine("Выводим исходный массив:");
+    PrintRandArray(array);
+    
+    SortRandArray(array);
+    
+    Console.WriteLine("Выводим отсортированный по модулю массив:");
+    PrintRandArray(array);
+
+    int[] FillRandArray(int[] array)
+    {
+        int i;
+        for (i = 0; i < array.Length; i++)
+        {
+            array[i] = rand.Next(-99, 100);
+        }
+        return array;
+    }
+
+    int[] SortRandArray(int[] array)
+    {
+        int temp;
+        int i;
+        int j;
+        for (i = 0; i < array.Length - 1; i++)
+        {
+
+            for (j = 0; j < array.Length -1 - i; j++)
+            {
+                if (Math.Abs(array[j]) > Math.Abs(array[j + 1]))
+                {
+                    temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+    void PrintRandArray(int[] array)
+    {
+        int i;
+        for (i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i]+ " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Zadacha29();
---
+++ Sem4/Program.cs
@@ -0,0 +1,82 @@
+﻿// Семинар 4
+// Задача 24. Напишите программу, которая принимает на вход число (А)
+// и выдаёт сумму чисел от 1 до А.
+/*Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"сумма чисел от 1 до {number} равна {FindSum(number)}");
+int FindSum(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+*/
+
+
+// Задача 26: Напишите программу, которая принимает на вход число
+// и выдаёт количество цифр в числе.
+
+/*Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"В числе {number}  {Numbers_K(number)} цифр(ы)");
+
+int Numbers_K(int number)
+{
+    int count;
+    for (count = 1; number / 10 > 0; count++)
+    {
+        number = number / 10;
+    }
+    return count;
+}
+*/
+
+// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+/*Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"произведение чисел от 1 до {number} равно {FindFac(number)}");
+
+int FindFac(int number)
+{
+    int fac = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        fac *= i;
+    }
+    return fac;
+}
+*/
+
+// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+//[1,0,1,1,0,1,0,0]
+
+
+void Zadacha30()
+{
+    int[] array = new int[8];
+    Random rand = new Random();
+    
+    int[] Fill_Zeros_Ones(int[] array)
+    {
+        int i;
+        for (i = 0; i < array.Length; i++)
+        {
+            array[i] = rand.Next(0,2);
+        }
+        return array;
+    }
+    void PrintArray(int[] array)
+    {
+        int j;
+        for (j = 0; j < array.Length; j++)
+        Console.Write(array[j]+ " ");
+    }
+    Fill_Zeros_Ones(array);
+    PrintArray(array);
+}
+
+Zadacha30();
