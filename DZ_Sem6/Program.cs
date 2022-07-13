d868c6a320d423d92419ced5a13d20bba9c45224
Vladimir_B <vladimir.baida@gmail.com>
2022-07-13 14:16:55 +0300

    Семинар 6 и решение ДЗ по нему

---
+++ DZ_Sem6/Program.cs
@@ -0,0 +1,67 @@
+﻿// ДЗ по семинару 6. Байда Владимир.
+// Задача 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+/*int M = 10; // количество чисел котороые мы будем вводить.
+Console.WriteLine($"Введенно {M} чисел, из них {CountPositiveNumbersInArray(CreateArray(M))} - больше нуля.");
+
+int[] CreateArray(int M)
+{
+    int[] arrayNumbers = new int[M];
+    for (int i=0; i < M; i++)
+    { 
+        Console.WriteLine("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        arrayNumbers[i] = number;
+    }
+    return arrayNumbers;
+}
+
+int CountPositiveNumbersInArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    { 
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+*/
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+// Задача 43.Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+/*Console.WriteLine("Введите значение параметра b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение параметра k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение параметра b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение параметра k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+LineIntersection(b1, k1, b2, k2);
+
+void LineIntersection(double b1, double k1, double b2, double k2)
+{
+    if (b1 != b2)
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = Math.Round((k1 * x + b1), 2);
+        x = Math.Round(x, 2);
+        Console.WriteLine($"Точка пересечения прямых, заданных уравнениями y = {k1}*x + {b1} и y = {k2}x + {b2}");
+        Console.WriteLine($"имеет координаты {x}:{y}");
+    } 
+    else 
+    {
+        Console.WriteLine($"Прямые, заданные уравнениями y = {k1}*x + {b1} и y = {k2}*x + {b2}");
+        Console.WriteLine($"не пересекаются, т.к. они паралельны.");
+    }
+}
+*/
+
+
---
+++ Sem6/Program.cs
@@ -0,0 +1,204 @@
+﻿// Семинар 6 по С#.
+
+//Задача 39: Напишите программу, которая перевернёт одномерный массив 
+//(последний элемент будет на первом месте, а первый - на последнем и т.д.)
+
+/*void Zadacha39()
+{   
+    int[] array = new int[10];
+    Random rand = new Random();
+    FillArray(array, 1, 10);
+    PrintArray(array);
+    ReverseArray(array);
+    PrintArray(array);
+
+
+    int[] FillArray(int[] array, int startNumber, int finishNumber)
+    {
+        
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rand.Next(startNumber, finishNumber);
+        }
+        return array;
+    }
+    int[] PrintArray(int[] array)
+    {
+        Console.WriteLine();
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i]);
+        }
+        return array;
+    }
+
+    int[] ReverseArray(int[] array)
+    {
+        for (int i = 0; i < array.Length / 2; i++)
+        {
+            (array[i], array[array.Length - 1 - i]) = (array[array.Length - 1 - i], array[i]);
+        }
+        return array;
+    }
+}
+Zadacha39 ();
+*/
+
+//Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, 
+//может ли существовать треугольник со сторонами такой длины.
+
+/*void Zadacha40()
+{   
+    IsTriangle(InputNumberA(), InputNumberB(), InputNumberC());
+
+    int InputNumberA()
+    {
+        Console.WriteLine("Введите первое число: ");
+        int numberA = Convert.ToInt32(Console.ReadLine());
+        return numberA;
+    }
+
+    int InputNumberB()
+    {
+        Console.WriteLine("Введите второе число: ");
+        int numberB = Convert.ToInt32(Console.ReadLine());
+        return numberB;
+    }
+    
+    int InputNumberC()
+    {
+        Console.WriteLine("Введите третье число: ");
+        int numberС = Convert.ToInt32(Console.ReadLine());
+        return numberС;
+    }
+
+    void IsTriangle(int numberA, int numberB, int numberC)
+    {
+        if (numberA + numberB > numberC && numberB + numberC > numberA && numberA + numberC > numberB)
+        {
+            Console.WriteLine($"треугольник со сторонами {numberA}, {numberB}, {numberC} существует!");
+        }
+        else
+        {
+              Console.WriteLine($"треугольник со сторонами {numberA}, {numberB}, {numberC} не существует!");
+        }
+
+    }
+}   
+
+Zadacha40 ();
+*/
+
+
+//Задача 42: Напишите программу, которая будет преобразовывать десятичные цифры в двоичные
+
+/*void Zadacha42()
+{   
+    Format(InputNumber());
+
+    int InputNumber()
+    {
+        Console.WriteLine("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        return number;
+    }
+
+   
+    void Format(int number)
+    {
+        int result = 0;
+        int bias = 1;
+        while (number >0)
+        {
+            result += number% 2 * bias;
+            bias *=10;
+            number /= 2; 
+        }
+        Console.WriteLine(result);
+    }
+}   
+
+Zadacha42 ();
+*/
+
+
+//Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+
+
+/*void Zadacha44()
+{   
+    Fibo(InputNumber());
+
+    int InputNumber()
+    {
+        Console.WriteLine("Введите число N: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        return number;
+    }
+   
+    void Fibo(int number)
+    {
+        int numberFirst = 0;
+         int numberSecond = 1;
+         int i;
+        for (i =0; i < number; i++)
+        {
+            Console.Write(numberFirst + "\t");
+            int temp = numberSecond;
+            
+            numberSecond = numberFirst + numberSecond;
+            numberFirst = temp;
+        }
+        
+    }
+}   
+
+Zadacha44 ();
+
+*/
+
+//Задача 45: Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+void Zadacha45()
+{   
+    int[] array = new int[10];
+    Random rand = new Random();
+    FillArray(array, 1, 10);
+    PrintArray(array);
+    DoubleArray(array);
+    PrintArray(DoubleArray(array));
+
+
+    int[] FillArray(int[] array, int startNumber, int finishNumber)
+    {
+        
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rand.Next(startNumber, finishNumber);
+        }
+        return array;
+    }
+    int[] PrintArray(int[] array)
+    {
+        Console.WriteLine();
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i]);
+        }
+        return array;
+    }
+
+    int[] DoubleArray(int[] array)
+    {
+        int[] newArray = new int[array.Length];
+        
+        
+        for (int i = 0; i < array.Length; i++)
+        {
+            newArray[i] = array[i];
+        }
+        return newArray;
+    }
+}
+Zadacha45 ();
+
